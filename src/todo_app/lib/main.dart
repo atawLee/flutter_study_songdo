8609713ed53844b721bd7a4b790baeba590c95b9
LeeJonghoon <34878017+atawLee@users.noreply.github.com>
2024-01-02 23:06:45 +0900

    Update main.dart
    todolist

--- src/todo_app/lib/main.dart
+++ src/todo_app/lib/main.dart
@@ -11,71 +11,142 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyWidget(),
+      home: TodoPage(),
     );
   }
 }
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class TodoPage extends StatefulWidget {
+  TodoPage({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<TodoPage> createState() => _TodoPageState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
-  bool isValue = true;
+class _TodoPageState extends State<TodoPage> {
+  List<String> list = [];
+  final controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
                   itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {},
-                      child: ListTile(
-                        leading:
-                            Checkbox(onChanged: (value) {}, value: isValue),
-                        title: Text(
-                          'TitleText',
-                          style:
-                              TextStyle(decoration: TextDecoration.lineThrough),
-                        ),
-                        trailing: IconButton(
-                          icon: Icon(Icons.delete),
-                          onPressed: () {},
+                    return SizedBox(
+                      height: 50,
+                      child: Card(
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (_) => PageB()));
+                              //_showMyDialog(context, list[index], list, index);
+                            });
+                          },
+                          child: Center(
+                            child: Text(list[index]),
+                          ),
                         ),
                       ),
                     );
                   },
-                  itemCount: 100),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Divider(),
-            ),
-            Row(
-              children: [
-                Expanded(child: TextField()),
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {},
-                )
-              ],
-            ),
-          ],
+                  itemCount: list.length,
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: controller,
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {});
+                      list.add(controller.text);
+                      controller.text = "";
+                    },
+                    child: Text("add"),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Future<void> _showMyDialog(
+      BuildContext context, String text, List<String> list, int index) async {
+    TextEditingController controller = TextEditingController(text: text);
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('할 일 수정/삭제'),
+          content: TextField(
+            controller: controller,
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () {
+                setState(() {
+                  list.removeAt(index);
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Update'),
+              onPressed: () {
+                setState(() {
+                  list[index] = controller.text;
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class PageB extends StatelessWidget {
+  const PageB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [],
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context); //뒤로가기
+            },
+            color: Colors.purple,
+            icon: Icon(Icons.arrow_back)),
+      ),
+      body: Center(child: Text('test')),
     );
   }
 }
